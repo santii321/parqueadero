59bbc27406bfcc2736c08336833b1960a0360d03
santii321 <iefaz.santiagogaviria@gmail.com>
2022-12-08 03:40:21 -0500

    se sube backend

--- parqueadero/src/App.js
+++ parqueadero/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../src/assets/style/index.css"
 import Layout from "./pages/Layout";
