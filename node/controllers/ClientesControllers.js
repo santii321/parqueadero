382142bdf111add1d5503b554a7020b4fa42a12b
ItsAristi02 <alejandraaristizabal49@gmail.com>
2022-12-09 21:16:59 -0500

    En el archivo (ClientesControllers), se crea la segunda estructura(CRUD) de codigo para mostrar solo un cliente mediante el id

--- node/controllers/ClientesControllers.js
+++ node/controllers/ClientesControllers.js
@@ -7,18 +7,29 @@ import clientesModel from "../models/ClientesModel.js";
 //*Mostrar todo los clientes
 
 const mostrarClientes = async (req, res) => {
-try {
-    const clientes= await clientesModel.findAll()
-    res.json(clientes)
-} catch (error) {
-    res.json({
-        message: `No se encontro ningun cliente ${error}`
-    })
-}
+    try {
+        const clientes = await clientesModel.findAll()
+        res.json(clientes)
+    } catch (error) {
+        res.json({
+            message: `La base de datos se encuentra vacia ${error}`
+        })
+    }
 }
 
 //*Mostrar un cliente
-
+const mostrarCliente = async (req, res) => {
+    try {
+        const cliente = await clientesModel.findOne({
+            where: { id: req.params.id }
+        })
+        res.json(cliente)
+    } catch (error) {
+        res.json({
+            message: `No se encontro ningun cliente con este id ${error}`
+        })
+    }
+}
 //*Crear cliente
 
 //*Editar cliente
