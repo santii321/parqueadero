4fd5ce8cb571f3e9ae3a0b842a9123cbb42cea20
Didiergalvis95 <didiergalvis95@hotmail.com>
2022-12-16 02:27:50 -0500

    Se agregan la expresiones regulares

--- parqueadero/src/pages/productos/EditarProductos.js
+++ parqueadero/src/pages/productos/EditarProductos.js
@@ -13,6 +13,15 @@ function EditarProductos(){
     const navigate = useNavigate();
     const {id} = useParams();
 
+    const expresionRegular = {
+        usuario: /^[a-zA-Z0-9\_]{4,16}$/, // Letras, numeros, guion_bajo
+        nombre: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+        password: /^.{4,12}$/, // 4 a 12 digitos
+        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        documento: /^\d{9,10}$/,
+        celular: /^\d{10}$/
+      };
+
     const updateProducto = async (e) =>{
         e.preventDefault();
         await axios.put(url + id, {
