2d6ed5a8e9820d5cceaa40210748873f875d7455
Didiergalvis95 <didiergalvis95@hotmail.com>
2022-12-15 21:08:54 -0500

    se crea boton para regresar a la pagina anterior

--- parqueadero/src/pages/productos/EditarProductos.js
+++ parqueadero/src/pages/productos/EditarProductos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 const url ='http://localhost:3100/productos/';
 
@@ -40,32 +40,40 @@ function EditarProductos(){
     }
 
     return(
-        <section className='w-100 d-flex  justify-content-center flex-wrap section-agregar'>
-            <h1 className='w-100 text-center'>Agregar producto al inventario</h1>
-            <form className='w-50 d-flex flex-column section-agregar__form'  onSubmit={updateProducto}>
-                <section class="input-group flex-nowrap">
-                    <span class="input-group-text" id="addon-wrapping">IMAGEN</span>
-                    <input class="form-control" value={imagen} onChange={ (e) => setImagen(e.target.value)}/>
-                </section>
-                <section class="input-group flex-nowrap">
-                    <span class="input-group-text" id="addon-wrapping">NOMBRE</span>
-                    <input class="form-control" value={nombre} onChange={ (e) => setNombre(e.target.value)}/>
-                </section>
-                <section class="input-group flex-nowrap">
-                    <span class="input-group-text" id="addon-wrapping">DESCRIPCION</span>
-                    <input class="form-control" value={descripcion} onChange={ (e) => setDescripcion(e.target.value)}/>
-                </section>
-                <section class="input-group flex-nowrap">
-                    <span class="input-group-text" id="addon-wrapping">PRECIO</span>
-                    <input class="form-control" value={precio} onChange={ (e) => setPrecio(e.target.value)}/>
-                </section>
-                <section class="input-group flex-nowrap">
-                    <span class="input-group-text" id="addon-wrapping">CANTIDAD</span>
-                    <input class="form-control" value={cantidad} onChange={ (e) => setCantidad(e.target.value)}/>
-                </section>
-                <button className='btns btn-almacenar' type='submit'>Agregar Cambio</button>
-            </form>
+        <section className='w-100'>
+            <section className="d-flex section-btns">
+                <Link className="btn btn-agregar" to="/Productos">
+                volver
+                </Link>
+            </section>
+            <section className='w-100 d-flex  justify-content-center flex-wrap section-agregar'>
+                <h1 className='w-100 text-center'>Agregar producto al inventario</h1>
+                <form className='w-50 d-flex flex-column section-agregar__form'  onSubmit={updateProducto}>
+                    <section class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">IMAGEN</span>
+                        <input class="form-control" value={imagen} onChange={ (e) => setImagen(e.target.value)}/>
+                    </section>
+                    <section class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">NOMBRE</span>
+                        <input class="form-control" value={nombre} onChange={ (e) => setNombre(e.target.value)}/>
+                    </section>
+                    <section class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">DESCRIPCION</span>
+                        <input class="form-control" value={descripcion} onChange={ (e) => setDescripcion(e.target.value)}/>
+                    </section>
+                    <section class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">PRECIO</span>
+                        <input class="form-control" value={precio} onChange={ (e) => setPrecio(e.target.value)}/>
+                    </section>
+                    <section class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">CANTIDAD</span>
+                        <input class="form-control" value={cantidad} onChange={ (e) => setCantidad(e.target.value)}/>
+                    </section>
+                    <button className='btns btn-almacenar' type='submit'>Agregar Cambio</button>
+                </form>
+            </section>
         </section>
+
     )
 }
 
