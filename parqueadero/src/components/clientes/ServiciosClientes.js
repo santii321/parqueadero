73d6f1fc3156f0cef6bd155576a01c14d469436f
Didiergalvis95 <didiergalvis95@hotmail.com>
2022-12-16 01:03:37 -0500

    se organizan estilos de la seccion clientes y los botones de regresar de las diferentes paginas

--- parqueadero/src/components/clientes/ServiciosClientes.js
+++ parqueadero/src/components/clientes/ServiciosClientes.js
@@ -7,59 +7,58 @@ import casco from "../../assets/images/iconcasco.png";
 
 const ServiciosClientes = () => {
     return (
-        <section class="center-layout--sm pb-6 pb-md-8 mx-auto full-width mt-5">
-            <div class="container mt-5 ">
+        <section class="sectionServicios center-layout--sm pb-6 pb-md-8 mx-auto full-width mt-5">
+            <section class="container mt-5 ">
 
+                <section class="contenedorServicios row row-cols-1 row-cols-sm-2 row-cols-md-3">
 
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-
-                    <div class="col-lg-4  d-flex flex-column justify-content-star align-items-center">
+                    <section class="col-lg-4  d-flex flex-column justify-content-star align-items-center">
                         <img src={motoLavado} alt="" class="bd-placeholder-img rounded-circle" width="75"
                             height="50" />
                         <h4 class="fw-normal text-white">Lavado de moto</h4>
                         <p class="text-center text-white"> Lavado y alistamiento, alto cilindraje $ 15 000 full, sencillo  $10 000, productos Meguiars.</p>
-                    </div>
+                    </section>
 
 
-                    <div class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
+                    <section class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
                     <img src={bateria} alt="" class="bd-placeholder-img rounded-circle" width="50"
                             height="50" />
                         <h4 class="fw-normal text-white">Peritaje</h4>
                         <p class="text-center text-white">Se realiza alineación y desfases del chasis, escaneo del motor y se mide espesor de pintura.</p>
-                    </div>
+                    </section>
 
-                    <div class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
+                    <section class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
                         <img src={casco} alt="" class="bd-placeholder-img rounded-circle" width="50"
                             height="50" I />
                         <h4 class="fw-normal text-white">Guardado de casco </h4>
                         <p class="text-center text-white">Se presta el servicio de guardado de cascos, en casilleros con el número de placa del vehículo</p>
 
-                    </div>
+                    </section>
 
-                    <div class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
+                    <section class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
                         <img src={llave} alt="" class="bd-placeholder-img rounded-circle" width="50"
                             height="50" />
                         <h4 class="fw-normal text-white">Mantenimiento</h4>
                         <p class="text-center text-white">Servicio técnico especializado en marcas BMW, DUCATI, KTM, HD, YAMAHA, SUZUKI, HONDA, KAWASAKI.</p>
-                    </div>
+                    </section>
 
-                    <div class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
+                    <section class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
                         <img src={dinero} alt="" class="bd-placeholder-img rounded-circle" width="50"
                             height="50" />
                         <h4 class="fw-normal text-white">Financiamiento</h4>
                         <p class="text-center text-white"> Financiación de la compra de nuestros equipos a través del sistema financiero y
                             en especial una estrategia de renting tecnológico. </p>
-                    </div>
+                    </section>
 
-                    <div class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
+                    <section class="col-lg-4 d-flex flex-column justify-content-star align-items-center">
                     <img src={aceite} alt="" class="bd-placeholder-img rounded-circle" width="50"
                             height="50" />
                         <h4 class="fw-normal text-white">Cambio de aceite</h4>
                         <p class="text-center text-white"> Este proceso permite que todos los engranajes de la moto funcionen correctamente.
                             Además, permite limpiar el motor y lo protege de la corrosión. </p>
-                    </div>
-                </div>
-            </div>
+                    </section>
+                </section>
+            </section>
         </section>
     )
 }
