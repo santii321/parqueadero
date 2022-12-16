3654f955e81053f232819da8c0a4b4a14b7ff69f
Didiergalvis95 <didiergalvis95@hotmail.com>
2022-12-16 00:26:30 -0500

    se agrega boton volver a vista agregar y editar

--- parqueadero/src/pages/productos/AgregarProductos.js
+++ parqueadero/src/pages/productos/AgregarProductos.js
@@ -1,14 +1,20 @@
+import { Link } from "react-router-dom";
 import "../../assets/style/productos/agregarProducto.css";
 import AgregarProducto from "../../components/Productos/AgregarProducto.js";
 
 function AgregarProductos() {
   return (
-    <>
+    <section className="w-100">
+      <section className="d-flex section-btns">
+        <Link className="btn btn-agregar" to="/Productos">
+          volver
+        </Link>
+      </section>
       <section className="w-100 d-flex  justify-content-center flex-wrap section-agregar">
         <h1 className="w-100 text-center">Agregar producto al inventario</h1>
         <AgregarProducto />
       </section>
-    </>
+    </section>
   );
 }
 export default AgregarProductos;
